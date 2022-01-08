4d293e2eaa6e8861a832089e4984aa8df791ec2b
DwayneL <dr2@ameritech.net>
2022-01-07 19:04:34 -0600

    removed malfunctioning form code for reset button

--- test.js
+++ test.js
@@ -3,13 +3,14 @@ const weight = document.getElementById("weight");
 const height = document.getElementById("height");
 const calcbtn = document.getElementById("addbutton");
 
+
 // calculating bmi with height and weight
 calcbtn.addEventListener("click", function calculate() {
   const weightNum = parseInt(weight.value);
   const heightNum = parseInt(height.value);
   const bmiCalc = ((weightNum / heightNum ** 2) * 703).toFixed(2);
   const parent = document.getElementById("parent");
-  const child = document.getElementById("child");
+  
 
   const newText1 = `Your BMI is ${bmiCalc}. You are normal weight`;
   const newText2 = `Your BMI is ${bmiCalc}. You are overweight.`;
@@ -36,3 +37,5 @@ calcbtn.addEventListener("click", function calculate() {
     document.getElementById("parent").appendChild(p);
   }
 });
+
+
